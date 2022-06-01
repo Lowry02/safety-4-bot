ee13a8322d1c02541d14f540782b851e747807c7
Lowry02 <lorenzocusin@MacBook-Air-di-Lorenzo.local>
2022-06-01 21:30:47 +0200

    adding thread

--- bot_safety.py
+++ bot_safety.py
@@ -1,10 +1,12 @@
 import datetime
 import requests
 import random
+from threading import Thread
 
 # creates date format. eg. 20220404
 def build_date():
-  date = datetime.datetime.now() + datetime.timedelta(days=1)
+  date = datetime.datetime.now()
+  # date = datetime.datetime.now() + datetime.timedelta(days=1)
   return date.strftime("%Y%m%d")
 
 # _first_hour_block = 1
@@ -12,6 +14,7 @@ _first_hour_block = 6
 # _last_hour_block = 5
 _last_hour_block = 10
 _hours = ["08", "11", "14", "17", "20"]
+
 _headers = {
   "origin" : "https://gosafety.web.app",
   "referer" : "https://gosafety.web.app/"
@@ -25,25 +28,33 @@ tokens = [
 ]
 
 #format => hour_block [6,7,8,9,10]
-booking_url = "https://us-central1-eiloborg.cloudfunctions.net/s4aapp2?f=prenota&token={}%3D&v1={}&v2=ED1&v3=^SER12^"
+booking_url = "https://us-central1-eiloborg.cloudfunctions.net/s4aapp2?f=prenota&token={}%3D&v1={}&v2=ED1&v3=Edificio%20A%20SPAZIO%20STUDIO%201%20piano%20terra%20ala%20sx^SER12^{}"
 
 # format => date, hour, booking_number
 access_url = "https://us-central1-eiloborg.cloudfunctions.net/s4aapp2?f=accedi&token={}%3D&v1={}*SER12*{}00*PRENOTAZIONE%20POSTO*Edificio%20A%20SPAZIO%20STUDIO%201%20piano%20terra%20ala%20sx%20%23{}"
 
 
-# booking rooms and accessing rooms for each user
-for token in tokens:
-  # booking rooms: from 08.00 to 17.00
-  for i in range(_first_hour_block, _last_hour_block + 1):
-    print(f"Booking {booking_url.format(token, i)}")
-    r = requests.get(booking_url.format(token, i), headers=_headers)
-    print(f"Result: {r.text}")
+def book_function(_first_hour_block, _last_hour_block):
+  # booking rooms and accessing rooms for each user
+  for token in tokens:
+    # booking rooms: from 08.00 to 17.00
+    for i in range(_first_hour_block, _last_hour_block + 1):
+      booking_number = random.randint(0, 104)
+      print(f"Booking {booking_url.format(token, i, booking_number)}")
+      r = requests.get(booking_url.format(token, i, booking_number), headers=_headers)
+      print(f"Result: {r.text}")
 
-  # accessing rooms
-  # for i in range(0, _last_hour_block):
-  #   date = build_date()
-  #   hour = _hours[i]
-  #   booking_number = random.randint(0, 104)
-  #   print(f"Accessing {access_url.format(token, date, hour, booking_number)}")
-  #   r = requests.get(access_url.format(token, date, hour, booking_number), headers=_headers)
-  #   print(f"Response: {r.text}")
+    # accessing rooms
+    # for i in range(0, _last_hour_block):
+    #   date = build_date()
+    #   hour = _hours[i]
+    #   booking_number = random.randint(0, 104)
+    #   print(f"Accessing {access_url.format(token, date, hour, booking_number)}")
+    #   r = requests.get(access_url.format(token, date, hour, booking_number), headers=_headers)
+    #   print(f"Response: {r.text}")
+
+t1 = Thread(target=book_function, args=(1,5))
+t2 = Thread(target=book_function, args=(5,10))
+
+t1.start()
+t2.start()
